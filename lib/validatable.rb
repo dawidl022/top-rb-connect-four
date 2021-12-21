7bf9a7ab187b78d7f7bec31a8e3c6ef1d801753d
dawidl022 <domdaw7@gmail.com>
2021-12-21 08:26:41 +0000

    Add new validatable method

--- lib/validatable.rb
+++ lib/validatable.rb
@@ -21,15 +21,33 @@ module Validatable
     valid_input
   end
 
-  def input_int_between(prompt, error_message, min, max)
+  def input_int_between(prompt, min, max,
+    error_message = "Please enter an integer between #{min} and #{max}.")
     user_input = nil
     loop do
       user_input = input_int(prompt, error_message)
       break if user_input >= min && user_input <= max
 
       puts error_message
+      put_blank_line
     end
 
     user_input
+  end
+
+  def ask_yes_no_question(prompt,
+    error_message = 'Allowed options are: "y" and "yes" for yes, ' \
+    'and "no" and "n" for no (all case-insensitive)')
+    answer = nil
+    loop do
+      print "#{prompt} [Y/n]: "
+      answer = gets.chomp.downcase
+      break if ['y', 'yes', 'n', 'no'].include?(answer)
+
+      puts error_message
+      put_blank_line
+    end
+
+    answer == 'y' || answer == 'yes'
   end
 end
--- spec/validatable_spec.rb
+++ spec/validatable_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../lib/validatable'
+require_relative 'util'
 
 RSpec.describe Validatable do
+  include Util
+
   REPEAT_BAD_INPUT_TIMES = 3
   ERROR_MESSAGE = "Invalid input: please enter an integer number.\n\n"
   DEFAULT_NUMBER = 3
+  let(:dummy_class) { Class.new { extend Validatable } }
 
   describe '#input_int' do
-    let(:dummy_class) { Class.new { extend Validatable } }
-
     before do
       allow(dummy_class).to receive(:gets).and_return("#{DEFAULT_NUMBER}\n")
     end
@@ -82,6 +84,159 @@ RSpec.describe Validatable do
       context 'accept other characters without leading digits' do
         it_behaves_like 'invalid input', 'edf'
       end
+    end
+
+    context 'when given a custom error message' do
+      before do
+        allow(dummy_class).to receive(:gets)
+          .and_return('aa', "#{DEFAULT_NUMBER}")
+      end
+      error_message = "You did not enter a valid integer, try again!"
+
+      it 'prints the error message when invalid input is given' do
+        expect { dummy_class.input_int('', error_message) }.to output(
+          "#{error_message}\n\n"
+        ).to_stdout
+      end
+    end
+  end
+
+  describe "#input_int_between" do
+    context 'when correct value is entered first time round' do
+      before { allow(dummy_class).to receive(:gets).and_return('7') }
+
+      it 'returns the input value' do
+        expect(dummy_class.input_int_between('', 1, 10)).to eq(7)
+      end
+    end
+
+    context 'when an incorrect value is entered first time round' do
+      before { allow(dummy_class).to receive(:gets).and_return('102', '7') }
+
+      it 'displays the default error message' do
+        expect { dummy_class.input_int_between('', 1, 10) }.to output(
+          "Please enter an integer between 1 and 10.\n\n"
+        ).to_stdout
+      end
+
+      it 'returns the corrected input value' do
+        mute_io(dummy_class)
+        expect(dummy_class.input_int_between('', 1, 10)).to eq(7)
+      end
+    end
+
+    context 'when a custom error message is supplied' do
+      before { allow(dummy_class).to receive(:gets).and_return('102', '7') }
+
+      error_message = "Out of range!"
+
+      it 'displays the error message when input is invalid' do
+        expect { dummy_class.input_int_between('', 1, 10, error_message) }
+          .to output("#{error_message}\n\n").to_stdout
+      end
+    end
+  end
+
+  describe "#ask_yes_no_question" do
+    let(:question) { "Do you like ruby?" }
+    before { allow(dummy_class).to receive(:gets).and_return('y') }
+
+    shared_examples_for 'answer is yes' do
+      it 'returns true' do
+        mute_io(dummy_class)
+        expect(dummy_class.ask_yes_no_question(question)).to be true
+      end
+    end
+
+    shared_examples_for 'answer is no' do
+      it 'returns false' do
+        mute_io(dummy_class)
+        expect(dummy_class.ask_yes_no_question(question)).to be false
+      end
+    end
+
+    context 'when y is supplied as input' do
+      before do
+        allow(dummy_class).to receive(:gets).and_return('y')
+      end
+
+      it_behaves_like 'answer is yes'
+    end
+
+    context 'when yes is supplied as input' do
+      before do
+        allow(dummy_class).to receive(:gets).and_return('yes')
+      end
+
+      it_behaves_like 'answer is yes'
+    end
+
+    context 'when YES is supplied as input' do
+      before do
+        allow(dummy_class).to receive(:gets).and_return('YES')
+      end
+
+      it_behaves_like 'answer is yes'
+    end
+
+    context 'when N is supplied as input' do
+      before do
+        allow(dummy_class).to receive(:gets).and_return('N')
+      end
+
+      it_behaves_like 'answer is no'
+    end
+
+    context 'when No is supplied as input' do
+      before do
+        allow(dummy_class).to receive(:gets).and_return('No')
+      end
+
+      it_behaves_like 'answer is no'
+    end
+
+    context 'when no is supplied as input' do
+      before do
+        allow(dummy_class).to receive(:gets).and_return('no')
+      end
+
+      it_behaves_like 'answer is no'
+    end
+
+    context 'when invalid input is supplied first, then valid input' do
+      before do
+        allow(dummy_class).to receive(:gets).and_return('yup', 'y')
+      end
+
+      error_message = 'Allowed options are: "y" and "yes" for yes, ' \
+        'and "no" and "n" for no (all case-insensitive)'
+
+      it 'displays the default error message' do
+        expect(dummy_class).to receive(:puts).with(error_message).once
+        expect(dummy_class).to receive(:puts).once
+        mute_io(dummy_class, [:print])
+        dummy_class.ask_yes_no_question('')
+      end
+
+      it_behaves_like 'answer is yes'
+    end
+
+    context 'when supplied with a custom error message' do
+      error_message = "Only y/yes/n/no are accepted!"
+
+      it 'displays the error message when input is invalid' do
+        allow(dummy_class).to receive(:gets).and_return('blah', 'n')
+        expect(dummy_class).to receive(:puts).with(error_message).once
+        expect(dummy_class).to receive(:puts).once
+        mute_io(dummy_class, [:print])
+        dummy_class.ask_yes_no_question('', error_message)
+      end
+    end
+
+    it 'prints the prompt' do
+      expect { dummy_class.ask_yes_no_question(question) }.to output(
+        "#{question} [Y/n]: "
+      ).to_stdout
     end
   end
 end
